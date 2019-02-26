85b0a32d5d4066092b1b8d3c20291e7548f7e944
Bryan Kegley <hi@bryankegley.me>
2019-02-25 18:41:37 -0600

    add Footer

--- packages/bryankegley.me/src/components/Footer.js
+++ packages/bryankegley.me/src/components/Footer.js
@@ -1,11 +1,37 @@
 import React from 'react'
-import {Box} from 'rebass'
+import {Link} from 'gatsby'
+import {Flex, Text} from 'rebass'
+import GitHub from 'react-feather/dist/icons/github'
+import Twitter from 'react-feather/dist/icons/twitter'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const LinkBox = styled(Flex)`
+  border: 2px solid ${props => props.theme.colors.black};
+  border-radius: 0.25rem;
+  padding: 8px;
+  :hover {
+    border-color: ${props => props.theme.colors.primary}
+    color: ${props => props.theme.colors.primary};
+  }
+`
+
+const FooterWrapper = styled(Flex)`
+  margin-left: auto;
+  margin-right: auto;
+`
 
 const Footer = () => {
   return (
-    <Box>
-      <h3>This is the footer</h3>
-    </Box>
+    <FooterWrapper width={['100%', '100%', '75%', '75%']} alignItems="center" justifyContent="center" py={6}>
+      <StyledLink to="/">
+        <Text>- bk</Text>
+      </StyledLink>
+    </FooterWrapper>
   )
 }
 
