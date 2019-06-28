9b5dcdf46417da08eadf61f9b2c87539d282392b
Bryan Kegley <hi@bryankegley.me>
2019-06-28 14:49:17 -0500

    add analytics

--- packages/bryankegley.me/gatsby-config.js
+++ packages/bryankegley.me/gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-142995105-1',
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
