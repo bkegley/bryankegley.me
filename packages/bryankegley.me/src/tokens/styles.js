deea7122863ef4aa94f3b73ca4595884585aa43c
Bryan Kegley <hi@bryankegley.me>
2019-07-09 07:23:57 -0500

    update styles

--- packages/bryankegley.me/src/tokens/styles.js
+++ packages/bryankegley.me/src/tokens/styles.js
@@ -6,6 +6,24 @@ const styles = {
     height: '100%',
     width: '100%',
     fontFamily: 'sans',
+    fontSize: 2,
+    lineHeight: 1.65,
+  },
+  h1: {
+    fontFamily: 'Aileron',
+    fontWeight: 700,
+    fontSize: [5, 6, '80px', '100px'],
+  },
+  h2: {
+    fontFamily: 'Aileron',
+    fontWeight: 100,
+    fontSize: [6, 6, 7, 7],
+  },
+  h3: {
+    fontFamily: 'Lato',
+    fontWeight: 400,
+    fontSize: 5,
+    color: 'primary',
   },
 }
 
