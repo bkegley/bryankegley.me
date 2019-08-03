1be1ce019cd1f6667cdbf4632eadd2f2f61454df
Bryan Kegley <hi@bryankegley.me>
2019-08-03 14:54:38 -0500

    add syntax highlighting

--- packages/bryankegley.me/gatsby-ssr.js
+++ packages/bryankegley.me/gatsby-ssr.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import {ThemeProvider, Styled} from 'theme-ui'
 import theme from './src/tokens/theme'
+import Prism from '@theme-ui/prism'
 
 import 'typeface-lato'
+import 'typeface-aileron'
+
+const components = {
+  pre: ({children}) => <>{children}</>,
+  code: Prism,
+}
 
 export const wrapRootElement = ({element}) => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme} components={components}>
       <Styled.root>{element}</Styled.root>
     </ThemeProvider>
   )
