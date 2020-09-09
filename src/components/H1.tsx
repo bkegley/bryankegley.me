91cdbb2e716700774214e0854909c1160aa54589
Scott Agirs <ScottAgirs@users.noreply.github.com>
2020-09-09 13:47:06 -0400

    Tweak spacing on H1 and pre /> (#22)
    * [change] H1 line-height to 1
    
    Signed-off-by: ScottAgirs <scott@ijs.to>
    
    * [add] margin-y 2rem on pre />
    
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -6,7 +6,7 @@ export interface H1Props {
 
 export const H1 = ({ children }: H1Props) => {
   return (
-    <h1 className="text-primary font-bold text-5xl lg:text-8xl my-4 font-heading">
+    <h1 className="text-primary font-bold text-5xl lg:text-8xl my-4 font-heading leading-none">
       {children}
     </h1>
   );
